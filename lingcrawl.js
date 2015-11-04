00d665bb0d5cba285414ad65f29a7225722bb58d
Michael Yoshitaka Erlewine <mitcho@mitcho.com>
2015-11-05 00:28:30 +0800

    refactor to download v-files, not current.pdf - surprise! LingBuzz actually serves files with correct modification dates if they're v-files; this should mean that wget timestamp functionality should work properly for v-files, though I haven't tested yet. - LingBuzz current version actually internally uses a v-file too, apparently - works by making dl take a callback; use cheerio to parse index html, then download each available v-file - I *think* this means non-PDF files are now downloaded properly too

--- lingcrawl.js
+++ lingcrawl.js
@@ -7,7 +7,9 @@
 // for i in {1..2742}; do node lingcrawl.js $i; done;
 
 var fs = require("fs"),
-	exec = require('child_process').exec;
+	exec = require('child_process').exec,
+	cheerio = require('cheerio'),
+	path = require('path');
 
 const LINGBUZZ = 'http://ling.auf.net/lingbuzz',
 	HEADERS = {'User-Agent': 'lingcrawl'},
@@ -17,25 +19,22 @@ function pad(n, digits) {
 	n += '';
 	return new Array(digits - n.length + 1).join('0') + n;
 }
-function dl(id, type) { // type = pdf | html
+function dl(id, filename, cb) { // filename = current.pdf | index.html | etc. (optional ?_s=)
 	id = pad(id, 6);
-	var filename;
-	var targetpath = LINGBUZZ + '/' + id;
-	var archivedir = ARCHIVEPATH + id;
-
+	var targetpath = LINGBUZZ + '/' + id + '/';
+	var archivedir = ARCHIVEPATH + id + '/';
+	
 	if ( !fs.existsSync(archivedir) )
 		fs.mkdirSync(archivedir);	
-	
-	if ( type == 'pdf' )
-		filename = '/current.pdf';
-	if ( type == 'html' )
-		filename = '/index.html';
+
 	targetpath += filename;
 
 	// don't redownload existing files
 	// todo: skip this for updated files? The issue is that lingbuzz doesn't seem to send back Last-Modified, so we can't do this intelligently.
-	if ( fs.existsSync(archivedir + filename) )
-		return;
+	if ( fs.existsSync(archivedir + filename) ) {
+		if ( typeof cb == 'function' )
+			return cb( id, filename, archivedir + filename );
+	}
 	
 	var flags = process.argv.length > 3 ? process.argv[3] : '-N';
 
@@ -44,14 +43,41 @@ function dl(id, type) { // type = pdf | html
 	var child = exec('wget ' + flags + ' -P ' + archivedir + ' -w 1m --random-wait ' + targetpath, function(err, stdout, stderr) {
 		if (err) {
 			if ( err.code == 8 ) // wget exit code 8 = server error
-				console.log('🚫  ' + id + ' ' + type);
+				console.log('🚫  ' + id + ' ' + filename);
 			else
 				console.log(err);
+		} else {
+			console.log('✅  ' + id + ' ' + filename);
+			if ( typeof cb == 'function' )
+				return cb( id, filename, archivedir + filename );
 		}
-		else console.log('✅  ' + id + ' ' + type);
-	});	
+	});
 }
 
 var id = process.argv[2];
-dl(id,'html');
-dl(id,'pdf');
+dl(id, 'index.html', function ( id, filename, filepath ) {
+	// load cheerio, the faux-jQuery
+	var $ = cheerio.load(fs.readFileSync(filepath));
+
+	var current = $('table tr:nth-child(1) > td:nth-child(2) a').attr('href');
+	var currentfile = path.basename(current);
+	var currentext = path.extname(current).replace(/\?.*$/,'');
+	var previousList = $('table tr:nth-child(5) > td:nth-child(2) a');
+	var maxrev = 0;
+	previousList.each(function(i, previous){
+		var link = $(previous).attr('href');
+		var revfile = link.replace(/^\/lingbuzz\/(\d+)\/(v\d+\.\w+)(\?.*)?$/, '$2');
+		var revnum = parseInt(revfile.replace(/^v(\d+)\.\w+$/, '$1'));
+		
+		dl(id, revfile);
+		
+		maxrev = Math.max(maxrev, revnum);
+	});
+
+	maxrev++;
+	var currentfile = 'v' + maxrev + currentext;
+	console.log('current = ' + currentfile);
+
+	dl(id, currentfile);
+});
+
