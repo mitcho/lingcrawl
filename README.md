cfb5c7f25658350c85fbc4d0643a759c0e40abb7
Michael Yoshitaka Erlewine <mitcho@mitcho.com>
2016-03-05 02:58:39 +0800

    add full options parsing and queue-ing... works better except for dying once in a while

--- README.md
+++ README.md
@@ -6,21 +6,21 @@ Dedicated to the public domain, Michael Yoshitaka Erlewine <mitcho@mitcho.com> 2
 
 ## Prerequisites
 
-lingcrawl is written in JavaScript. Install node.js and the package `cheerio` via npm. The command-line tool `wget` is also required.
+lingcrawl is written in JavaScript. Install node.js and the npm packages `cheerio`, `async`, and `yargs`. The command-line tool `wget` is also required.
 
 ## Basic usage
 
-lingcrawl takes one argument which is a LingBuzz ID number. It does not have to be padded with extra zeros.
+lingcrawl takes minimally one argument which is a LingBuzz ID. It does not have to be padded with extra zeros.
 
 	node lingcrawl.js 2210
 
 This will download the HTML index file for LingBuzz #002210, then download all revisions including the current revision. The results will be in `archive/002210`.
 
-To run it over a range of numbers, just use a shell for loop, like the following in bash:
+To run it over a range of numbers, enter the first and last ID:
 
-	for i in {1..2754}; do node lingcrawl.js $i; done;
+	node lingcrawl.js 1 2746
 
-Because some downloads may fail (when LingBuzz is down), it may be necessary to make multiple passes through.
+If any downloads fail along the way, the script will try to requeue the download for a little later and try again. If the server never comes back online, the script may not terminate.
 
 ## Syncing
 
--- lingcrawl.js
+++ lingcrawl.js
@@ -3,10 +3,38 @@
 // Dedicated to the public domain, 2015
 // http://github.com/mitcho/lingcrawl
 
+// option parsing using yargs:
+var argv = require('yargs')
+	.usage('Usage: $0 <start ID> (<end ID>) [options]')
+	.demand(1) // require an ID
+	.option('c', {
+		alias: 'concurrent',
+		default: 10
+	})
+	.option('v', {
+		alias: 'verbose',
+		default: false,
+		boolean: true
+	})
+	.option('f', {
+		alias: 'forceindex',
+		default: false,
+		boolean: true,
+		describe: 'Ignore cached index.html files'
+	})
+	.option('F', {
+		alias: 'forceall',
+		default: false,
+		boolean: true,
+		describe: 'Ignore all cached files'
+	})
+	.help('help')
+	.argv;
+
 var fs = require("fs"),
 	exec = require('child_process').exec,
 	cheerio = require('cheerio'),
-	path = require('path')
+	path = require('path'),
 	async = require('async');
 
 const LINGBUZZ = 'http://ling.auf.net/lingbuzz',
@@ -14,102 +42,104 @@ const LINGBUZZ = 'http://ling.auf.net/lingbuzz',
 	ARCHIVEPATH = './archive/',
 	CLEANHTML = "sed -i '' -E -e 's/\\?_s=[A-Za-z0-9_-]+(&amp;_k=[A-Za-z0-9_-]+&amp;[0-9])?\\\"/\\\"/g' ";
 
-CONCURRENCY = 5;
+var start = argv._[0];
+var end = (argv._.length > 1) ? argv._[1] : start;
 
 function pad(n, digits) {
 	n += '';
 	return new Array(digits - n.length + 1).join('0') + n;
 }
 
-function dl(id, filename, cb) { // filename = current.pdf | index.html | etc. (optional ?_s=)
+var q = async.queue(function (task, callback) {
+	var id = task.id;
 	id = pad(id, 6);
 	var targetpath = LINGBUZZ + '/' + id + '/';
 	var archivedir = ARCHIVEPATH + id + '/';
-	
+
 	if ( !fs.existsSync(archivedir) )
 		fs.mkdirSync(archivedir);	
 
+	var filename = task.filename; // filename = current.pdf | index.html | etc. (optional ?_s=)
+
+	if (argv.v)
+		console.log(id, filename);
+	
 	targetpath += filename;
+
+	var cb;
+	if ( filename == 'index.html' ) {
+		cb = function () {
+			// load cheerio, the faux-jQuery
+			var $ = cheerio.load(fs.readFileSync(archivedir + filename));
+
+			var current = $('table tr:nth-child(1) > td:nth-child(2) a').attr('href');
+			var currentfile = path.basename(current);
+			var currentext = path.extname(current).replace(/\?.*$/,'');
+			var previousList = $('table tr:nth-child(5) > td:nth-child(2) a');
+			var maxrev = 0;
+			previousList.each(function(i, previous){
+				var link = $(previous).attr('href');
+				var revfile = link.replace(/^\/lingbuzz\/(\d+)\/(v\d+\.\w+)(\?.*)?$/, '$2');
+				var revnum = parseInt(revfile.replace(/^v(\d+)\.\w+$/, '$1'));
+
+				q.push({id: id, filename: revfile});
+		
+				maxrev = Math.max(maxrev, revnum);
+			});
+
+			maxrev++;
+			var currentfile = 'v' + maxrev + currentext;
+		// 	console.log('current = ' + currentfile);
+
+			q.push({id: id, filename: currentfile});
+
+			callback();
+		}
+	} else {
+		cb = callback;
+	}
 
 	// don't redownload existing files
 	// todo: skip this for updated files? The issue is that lingbuzz doesn't seem to send back Last-Modified, so we can't do this intelligently.
-	if ( fs.existsSync(archivedir + filename) ) {
-		if ( typeof cb == 'function' )
-			cb( id, filename, archivedir + filename );
-		return
+	if ( !argv.F && !(argv.f && filename == 'index.html') && fs.existsSync(archivedir + filename) ) {
+		return cb( id, filename, archivedir + filename );
 	}
 	
-	var flags = process.argv.length > 3 ? process.argv[3] : '-N';
+	// todo: add flag to add wget flag -N ?
 
 	console.log('🐌  ' + targetpath + ' ...');
 
-	var child = exec('wget ' + flags + ' -P ' + archivedir + ' -w 1m --random-wait ' + targetpath, function(err, stdout, stderr) {
+	var child = exec('wget -P ' + archivedir + ' -w 1m --random-wait ' + targetpath, function(err, stdout, stderr) {
 		if (err) {
-			if ( err.code == 8 ) // wget exit code 8 = server error
-				console.log('🚫  ' + id + ' ' + filename);
-			else
+			if ( err.code == 8 ) { // wget exit code 8 = server error
+				console.log('🚫  ' + id + ' ' + filename + "\trequeuing...");
+				q.push({id: id, filename: filename});
+			} else
 				console.log(err);
-			return;
+			return callback();
 		}
 
 		console.log('✅  ' + id + ' ' + filename);
-
-		var next = function() {
-			if ( typeof cb == 'function' )
-				return cb( id, filename, archivedir + filename );
-		}
 
 		if ( filename == 'index.html' ) { // index.html files need to be cleaned up
 			exec(CLEANHTML + archivedir + filename, function(err, stdout, stderr) {
 				if (err)
 					console.log('⚠️  ' + id + ' on sed: ', err);
-				next();
+				cb( id, filename );
 			});
 		} else {
-			next();
+			cb( id, filename );
 		}
 	});
-}
 
-var q = async.queue(function (task, callback) {
-	var id = task.id;
-
-	dl(id, 'index.html', function ( id, filename, filepath ) {
-		// load cheerio, the faux-jQuery
-		var $ = cheerio.load(fs.readFileSync(filepath));
-
-		var current = $('table tr:nth-child(1) > td:nth-child(2) a').attr('href');
-		var currentfile = path.basename(current);
-		var currentext = path.extname(current).replace(/\?.*$/,'');
-		var previousList = $('table tr:nth-child(5) > td:nth-child(2) a');
-		var maxrev = 0;
-		previousList.each(function(i, previous){
-			var link = $(previous).attr('href');
-			var revfile = link.replace(/^\/lingbuzz\/(\d+)\/(v\d+\.\w+)(\?.*)?$/, '$2');
-			var revnum = parseInt(revfile.replace(/^v(\d+)\.\w+$/, '$1'));
-		
-			dl(id, revfile);
-		
-			maxrev = Math.max(maxrev, revnum);
-		});
-
-		maxrev++;
-		var currentfile = 'v' + maxrev + currentext;
-	// 	console.log('current = ' + currentfile);
-
-		dl(id, currentfile, callback);
-	});
-}, CONCURRENCY);
+}, argv.c);
 
 // register final callback
 q.drain = function() {
     console.log('all items have been processed');
 }
-q.saturated = function() {
-	console.log('saturated the queue!');
-}
 
 // queue up IDs
-for ( var i = 1; i <= process.argv[2]; i++ ) {
-	q.push({id: i});
+for ( var i = start; i <= end; i++ ) {
+	q.push({id: i, filename: 'index.html'});
 }
