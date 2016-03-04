3810ceafd1037625aeca7794c3d92dcd0af05c9f
Michael Yoshitaka Erlewine <mitcho@mitcho.com>
2016-03-05 00:22:01 +0800

    first stab at better async control

--- lingcrawl.js
+++ lingcrawl.js
@@ -6,12 +6,15 @@
 var fs = require("fs"),
 	exec = require('child_process').exec,
 	cheerio = require('cheerio'),
-	path = require('path');
+	path = require('path')
+	async = require('async');
 
 const LINGBUZZ = 'http://ling.auf.net/lingbuzz',
 	HEADERS = {'User-Agent': 'lingcrawl'},
 	ARCHIVEPATH = './archive/',
 	CLEANHTML = "sed -i '' -E -e 's/\\?_s=[A-Za-z0-9_-]+(&amp;_k=[A-Za-z0-9_-]+&amp;[0-9])?\\\"/\\\"/g' ";
+
+CONCURRENCY = 5;
 
 function pad(n, digits) {
 	n += '';
@@ -68,30 +71,45 @@ function dl(id, filename, cb) { // filename = current.pdf | index.html | etc. (o
 	});
 }
 
-var id = process.argv[2];
-dl(id, 'index.html', function ( id, filename, filepath ) {
-	// load cheerio, the faux-jQuery
-	var $ = cheerio.load(fs.readFileSync(filepath));
+var q = async.queue(function (task, callback) {
+	var id = task.id;
 
-	var current = $('table tr:nth-child(1) > td:nth-child(2) a').attr('href');
-	var currentfile = path.basename(current);
-	var currentext = path.extname(current).replace(/\?.*$/,'');
-	var previousList = $('table tr:nth-child(5) > td:nth-child(2) a');
-	var maxrev = 0;
-	previousList.each(function(i, previous){
-		var link = $(previous).attr('href');
-		var revfile = link.replace(/^\/lingbuzz\/(\d+)\/(v\d+\.\w+)(\?.*)?$/, '$2');
-		var revnum = parseInt(revfile.replace(/^v(\d+)\.\w+$/, '$1'));
+	dl(id, 'index.html', function ( id, filename, filepath ) {
+		// load cheerio, the faux-jQuery
+		var $ = cheerio.load(fs.readFileSync(filepath));
+
+		var current = $('table tr:nth-child(1) > td:nth-child(2) a').attr('href');
+		var currentfile = path.basename(current);
+		var currentext = path.extname(current).replace(/\?.*$/,'');
+		var previousList = $('table tr:nth-child(5) > td:nth-child(2) a');
+		var maxrev = 0;
+		previousList.each(function(i, previous){
+			var link = $(previous).attr('href');
+			var revfile = link.replace(/^\/lingbuzz\/(\d+)\/(v\d+\.\w+)(\?.*)?$/, '$2');
+			var revnum = parseInt(revfile.replace(/^v(\d+)\.\w+$/, '$1'));
 		
-		dl(id, revfile);
+			dl(id, revfile);
 		
-		maxrev = Math.max(maxrev, revnum);
+			maxrev = Math.max(maxrev, revnum);
+		});
+
+		maxrev++;
+		var currentfile = 'v' + maxrev + currentext;
+	// 	console.log('current = ' + currentfile);
+
+		dl(id, currentfile, callback);
 	});
+}, CONCURRENCY);
 
-	maxrev++;
-	var currentfile = 'v' + maxrev + currentext;
-// 	console.log('current = ' + currentfile);
+// register final callback
+q.drain = function() {
+    console.log('all items have been processed');
+}
+q.saturated = function() {
+	console.log('saturated the queue!');
+}
 
-	dl(id, currentfile);
-});
-
+// queue up IDs
+for ( var i = 1; i <= process.argv[2]; i++ ) {
+	q.push({id: i});
+}
